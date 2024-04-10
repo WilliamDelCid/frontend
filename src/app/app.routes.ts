08f8e35a5684f102d358c22ae0b8a67945623f84
williamdelcid <dm18019@ues.edu.sv>
2024-04-09 20:36:01 -0600

    Agregando ng boostrap and guards component login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { SkeletonComponent } from './shared/components/layouts/skeleton/skeleton.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './core/guards/login.guard';
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: SkeletonComponent,
+    component: LoginComponent,
+    canActivate:[LoginGuard]
   }
 ];
