fe6933e6976242670d5259f5324e022bc5f37647
williamdelcid <dm18019@ues.edu.sv>
2024-04-11 22:56:26 -0600

    Agregando CRUD Inventario

--- src/app/core/services/inventario.service.ts
+++ src/app/core/services/inventario.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { RequestResponse } from '../models/IRequest';
-import { Inventario } from '../../pages/interfaces/inventario.interface';
+import { Inventario, TipoProducto, Unidad } from '../../pages/interfaces/inventario.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventarioService {
   private url = environment.urlAPI + 'inventarios';
+  private urlUnidad = environment.urlAPI + 'unidad';
+  private urlTipoProducto = environment.urlAPI + 'tipos-producto';
 
   isLoading = false;
 
@@ -40,15 +43,19 @@ export class InventarioService {
     totalElements: 0,
     totalPages: 0,
   };
+  listUnidad: Unidad[] = [];
+  listTipoProducto: TipoProducto[] = [];
+
   constructor(private inventarioService: HttpClient) {}
 
-  getPages(page: number = 0, size: number = 1) {
+  getPages(page: number = 0, size: number = 10,nombreProducto: string = '') {
     this.isLoading = true;
     return this.inventarioService
       .get<RequestResponse<Inventario>>(this.url , {
         params: {
           page: page.toString(),
           size: size.toString(),
+          nombreProducto: nombreProducto
         },
       })
       .subscribe((data) => {
@@ -56,4 +63,33 @@ export class InventarioService {
         this.isLoading = false;
       });
   }
+
+  getUnidad() {
+    return this.inventarioService.get<Unidad[]>(`${this.urlUnidad}`).subscribe((resp) => {
+      this.listUnidad = resp;
+    });
+  }
+
+  getTipoProducto() {
+    return this.inventarioService.get<TipoProducto[]>(`${this.urlTipoProducto}`).subscribe((resp) => {
+      this.listTipoProducto = resp;
+    });
+  }
+
+  crear(inventario: FormGroup) {
+    return this.inventarioService.post(`${this.url}`, inventario);
+  }
+
+  actualizar(idInventario: number, inventario: FormGroup) {
+    return this.inventarioService.put(`${this.url}/${idInventario}`, inventario);
+  }
+
+  eliminar(idInventario: number) {
+    return this.inventarioService.delete(`${this.url}/${idInventario}`);
+  }
+
+  buscar(idInventario: number) {
+    return this.inventarioService.get<Inventario>(`${this.url}/${idInventario}`);
+  }
+
 }
--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbModal, NgbModalRef, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import {
+  NgbModal,
+  NgbModalRef,
+  NgbModule,
+  NgbPaginationModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { InventarioService } from '../../core/services/inventario.service';
 import Swal from 'sweetalert2';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule } from '@angular/common';
+import { Inventario } from '../interfaces/inventario.interface';
 
 const DEFAULT_PAGE_NUMBER = 1;
 @Component({
   selector: 'app-inventario',
   standalone: true,
-  imports: [NgbPaginationModule],
+  imports: [
+    NgSelectModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.scss',
 })
@@ -17,7 +39,7 @@ export class InventarioComponent implements OnInit {
   modalRef: NgbModalRef | undefined;
   formularioGeneral!: FormGroup;
   editar: boolean = false;
-
+  nombreProducto: string = '';
   constructor(
     private inventarioService: InventarioService,
     private modalService: NgbModal,
@@ -26,6 +48,8 @@ export class InventarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.inventarioService.getPages();
+    this.inventarioService.getUnidad();
+    this.inventarioService.getTipoProducto();
     this.formularioGeneral = this.iniciarFormularioGeneral();
   }
 
@@ -36,8 +60,7 @@ export class InventarioComponent implements OnInit {
       descripcion: ['', [Validators.required]],
       producto: ['', [Validators.required]],
       unidad: ['', [Validators.required]],
-      cantidadProducto: ['', [Validators.required]],
-      estadoProducto: ['', [Validators.required]],
+      cantidadProducto: ['', [Validators.required, this.noNegativoValidator()]],
       tipoProducto: ['', [Validators.required]],
     });
   }
@@ -54,15 +77,34 @@ export class InventarioComponent implements OnInit {
     return this.inventarioService.listInventario;
   }
 
+  get unidades() {
+    return this.inventarioService.listUnidad;
+  }
+
+  get TipoProductos() {
+    return this.inventarioService.listTipoProducto;
+  }
 
   openModal(content: TemplateRef<unknown>, idInventario: number = 0) {
     this.formularioGeneral.reset();
-
     if (idInventario) {
-      this.editar = true;
-      this.modalService.open(content, {
-        size: 'lg',
-        centered: true,
+      Swal.fire({
+        text: 'Por favor, espere un momento...',
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+        returnFocus: false,
+      });
+
+      this.inventarioService.buscar(idInventario).subscribe((resp) => {
+        this.llenarFormulario(resp);
+        this.editar = true;
+        Swal.close();
+        this.modalService.open(content, {
+          size: 'lg',
+          centered: true,
+        });
       });
     } else {
       this.editar = false;
@@ -73,22 +115,116 @@ export class InventarioComponent implements OnInit {
     }
   }
 
+  guardar() {
+    if (this.formularioGeneral.valid) {
+      if (this.formularioGeneral.value.id) {
+        this.inventarioService
+          .actualizar(
+            this.formularioGeneral.value.id,
+            this.formularioGeneral.value
+          )
+          .subscribe({
+            next: () => {
+              this.inventarioService.getPages();
+              this.modalService.dismissAll();
+              this.editar = false;
+              this.currentPage = DEFAULT_PAGE_NUMBER;
+            },
+            error: (error: any) => {
+              console.log(error);
+            },
+          });
+      } else {
+        this.inventarioService.crear(this.formularioGeneral.value).subscribe({
+          next: () => {
+            this.inventarioService.getPages();
+            this.modalService.dismissAll();
+            this.editar = false;
+            this.currentPage = DEFAULT_PAGE_NUMBER;
+          },
+          error: (error: any) => {
+            console.log(error);
+          },
+        });
+      }
+    }
+    return Object.values(this.formularioGeneral.controls).forEach((control) =>
+      control.markAsTouched()
+    );
+  }
+
+  close() {
+    this.modalService.dismissAll();
+  }
+
   eliminar(idInventario: number) {
     Swal.fire({
-        title: 'Aviso',
-        text: '¿Eliminar este producto?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, eliminar',
-        cancelButtonText: 'Cancelar'
+      title: 'Aviso',
+      text: '¿Eliminar este producto?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
     }).then((result) => {
-        if (result.isConfirmed) {
-
-        }
+      if (result.isConfirmed) {
+        this.inventarioService.eliminar(idInventario).subscribe({
+          next: () => {
+            this.inventarioService.getPages();
+            this.currentPage = DEFAULT_PAGE_NUMBER;
+            Swal.fire({
+              title: "Inventario Eliminado",
+              text: "Inventario Eliminado con exito",
+              icon: "success"
+            });
+          },
+          error: (error) => {
+            Swal.fire({
+              title: "Ocurrio un error al eliminar el inventario",
+              text: error.error.mensaje,
+              icon: "error"
+            });
+          },
+        })
+      }
     });
-}
+  }
 
+  esCampoValido(campo: string) {
+    const validarCampo = this.formularioGeneral.get(campo);
+    return !validarCampo?.valid && validarCampo?.touched
+      ? 'is-invalid'
+      : validarCampo?.touched
+      ? 'is-valid'
+      : '';
+  }
+
+  llenarFormulario(resp: Inventario): void {
+    const productoValue = resp.producto ? 'true' : 'false';
+    this.formularioGeneral.setValue({
+      id: resp.id,
+      nombreProducto: resp.nombreProducto,
+      descripcion: resp.descripcion,
+      producto: productoValue,
+      unidad: resp.unidad.id,
+      cantidadProducto: resp.cantidadProducto,
+      tipoProducto: resp.tipoProducto.id,
+    });
+  }
+
+  noNegativoValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const cantidad = control.value;
+      if (cantidad < 0) {
+        return { negativo: true };
+      }
+      return null;
+    };
+  }
+
+  buscar() {
+    this.inventarioService.getPages(0, 10, this.nombreProducto);
+  }
 
 }
